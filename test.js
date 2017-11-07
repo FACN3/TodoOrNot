4e41e5cc11e73abc8e40ba4c60c51a7b9a1d2c22
ionush <neilasjoseph@gmail.com>
2017-11-07 17:36:40 +0200

    working as a team to solve the first logic.js problems NO TESTING YET

--- test.js
+++ test.js
@@ -1,7 +1,49 @@
+var state = [
+  {
+    id: -3, title: 'first todo', description: 'first todo'
+    , complete: false
+  },
+  {
+    id: -2, title: 'second todo', description: 'second todo'
+    , complete: false
+  },
+  {
+    id: -1, title: 'third todo', description: 'third todo'
+    , complete: false
+  },
+];
+
+var state1 = [
+{
+    id: -3, title: 'first todo', description: 'first todo'
+    , complete: false
+  },
+  {
+    id: -2, title: 'second todo', description: 'second todo'
+    , complete: false
+  },
+  {
+    id: -1, title: 'third todo', description: 'third todo'
+    , complete: false
+  },
+  {
+    id: 0, title: 'fourth  todo', description: 'fourth todo'
+    , complete: false
+  },
+
+];
+
+var newState = {
+  id: 0, title: 'fourth  todo', description: 'fourth todo'
+  , complete: false
+};
+
 var test = require('tape');
 var logic = require('./logic');
 
-test('Example test', function(t) {
-  t.pass();
+tape('adding one object to existing array', function(t) {
+  var actual=todoFunctions.addTodo(state, newState);
+  var expected = state1
+  t.equal(actual,expected,'addTodo array should have 4 objects');
   t.end();
 });
