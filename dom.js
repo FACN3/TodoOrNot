002fa6349132b5a35aa909831f68ad7e418225c6
Amir K <amirkhourykh@gmail.com>
2017-11-09 12:48:29 +0200

    Revert "Revert "Deleted Consol log codes""

--- dom.js
+++ dom.js
@@ -5,7 +5,10 @@
   // This is the dom node where we will keep our todo
   var container = document.getElementById('todo-container');
   var addTodoForm = document.getElementById('add-todo');
-  console.log
+  // var editTodoForm = document.getElementsByClassName('edit-todo');
+  var editTodoForm = document.getElementsByClassName('edit-todo');
+
+
   var state = [
     {
       id: -3, title: 'first todo', description: 'first todo'
@@ -71,6 +74,35 @@
     addDetails.appendChild(addDescription);
     addDetails.appendChild(editButton);
     addDetails.appendChild(deleteButton);
+
+    var editTodo = document.createElement('form');
+    editTodo.id="edit-todo";
+    editTodo.classList.add('edit-todo');
+    var editTitle = document.createElement('input');
+    editTitle.type="text";
+    editTitle.id="editTitle"+todo.id;
+    editTodo.appendChild(editTitle);
+    var editDescription = document.createElement('input');
+    editDescription.type="text";
+    editDescription.id="editDescription"+todo.id;
+    editTodo.appendChild(editDescription);
+    var editButton = document.createElement('input');
+    editButton.type = "submit";
+    editButton.value = "edit";
+    editTodo.appendChild(editButton);
+
+    editButton.addEventListener('click', function (event) {
+
+      editObj={"id":todo.id,"title":document.getElementById('editTitle'+todo.id).value,
+      "description":document.getElementById('editDescription'+todo.id).value,
+      "complete": todo.complete}
+      event.preventDefault();
+      var newState = todoFunctions.editTodo(state, todo.id,editObj);
+      update(newState);
+
+    });
+
+    addDetails.appendChild(editTodo);
     todoNode.appendChild(addDetails);
 
     return todoNode;
