746ce0d5ec0edd1b6782a37e854f7f667db67a3a
idantb101 <idantb101@gmail.com>
2017-11-09 12:33:53 +0200

    Revert "Deleted commented out codes"

--- dom.js
+++ dom.js
@@ -4,8 +4,14 @@
 (function () {
   // This is the dom node where we will keep our todo
   var container = document.getElementById('todo-container');
+  console.log(container);
   var addTodoForm = document.getElementById('add-todo');
-  console.log
+  console.log(addTodoForm);
+  // var editTodoForm = document.getElementsByClassName('edit-todo');
+  var editTodoForm = document.getElementsByClassName('edit-todo');
+  console.log(editTodoForm)
+
+
   var state = [
     {
       id: -3, title: 'first todo', description: 'first todo'
@@ -40,6 +46,7 @@
 
 
     // add title
+    var editStatus
     var nodeTitle = document.createElement('h2');
     nodeTitle.classList.add('todoTitle');
     var titleText = document.createTextNode(todo.title);
@@ -51,11 +58,13 @@
     var addDescription = document.createTextNode(todo.description);
 
 
-    var editButton = document.createElement('button');
-    editButton.classList.add('editButton');
-    var editButtonText= document.createTextNode('edit');
-    editButton.appendChild(editButtonText);
+    // var editButton = document.createElement('button');
+    // editButton.classList.add('editButton');
+    // var editButtonText= document.createTextNode('edit');
+    // editButton.appendChild(editButtonText);
       //add editButton functionality
+
+
 
     var deleteButton = document.createElement('button');
     deleteButton.classList.add('deleteButton');
@@ -63,19 +72,97 @@
     deleteButton.appendChild(deleteButtonText);
     console.log(todo);
     deleteButton.addEventListener('click', function (event) {
-
+        event.preventDefault();
       var newState = todoFunctions.deleteTodo(state, todo.id);
       update(newState);
     });
+    //add deleteButton functionality
 
     addDetails.appendChild(addDescription);
-    addDetails.appendChild(editButton);
+    // addDetails.appendChild(editButton);
     addDetails.appendChild(deleteButton);
+
+    var editTodo = document.createElement('form');
+    editTodo.id="edit-todo";
+    editTodo.classList.add('edit-todo');
+    var editTitle = document.createElement('input');
+    editTitle.type="text";
+    editTitle.id="editTitle"+todo.id;
+    editTodo.appendChild(editTitle);
+    var editDescription = document.createElement('input');
+    editDescription.type="text";
+    editDescription.id="editDescription"+todo.id;
+    editTodo.appendChild(editDescription);
+    var editButton = document.createElement('input');
+    editButton.type = "submit";
+    editButton.value = "edit";
+    editTodo.appendChild(editButton);
+
+    editButton.addEventListener('click', function (event) {
+
+      editObj={"id":todo.id,"title":document.getElementById('editTitle'+todo.id).value,
+      "description":document.getElementById('editDescription'+todo.id).value,
+      "complete": todo.complete}
+      console.log(editObj);
+      event.preventDefault();
+      var newState = todoFunctions.editTodo(state, todo.id,editObj);
+      update(newState);
+
+    });
+
+    addDetails.appendChild(editTodo);
     todoNode.appendChild(addDetails);
 
+
+
+        //create event listener for button press
+        //insert new text box
+        //re write description into todo
+        // update new state with new description
+    // editButton.hidden = "true";
+    // editTodo.appendChild(editButton);
+    // todoNode.appendChild(editTodo);
+
+
+      // contains text boxx
+      // contains submit
+    // on the click of edit, replace current title and description with text box
+    // title and description in edit boxes replaces current node info
+
+
+    // this adds the delete button. PROVIDED FUNCTION
+    // var deleteButtonNode = document.createElement('button');
+    // deleteButtonNode.addEventListener('click', function (event) {
+    //   var newState = todoFunctions.deleteTodo(state, todo.id);
+    //   update(newState);
+    // });
+    // addDetails.appendChild(deleteButtonNode);
+    // add markTodo button
+    // add classes for css
     return todoNode;
   };
 
+  // if (editTodoForm) {
+  //   editTodoForm.forEach(function(form){
+  //
+  //     form.addEventListener('submit', function(event){
+  //       event.preventDefault();
+  //       var title = document.getElementById('editTitle').value;
+  //       document.getElementById('editTitle').value = '';
+  //       var description = document.getElementById('editDescription').value;
+  //       document.getElementById('editDescription').value = '';
+  //       console.log(title + description);
+  //       var newArray = {id : todo.id, title : title,
+  //         description : description, complete: todo.complete};
+  //         var newState = todoFunctions.editTodo(state,todo.id,newArray); // ?? change this!
+  //         update(newState);
+  //
+  //       });
+  //
+  //   })
+  // }
+
+  // bind create todo form
   if (addTodoForm) {
     addTodoForm.addEventListener('submit', function (event) {
       event.preventDefault();
@@ -87,7 +174,6 @@
       var description = document.getElementById('formDescription').value;
       document.getElementById('formDescription').value = '';
       console.log(title + description);
-
       var newArray = {id : todoFunctions.generateId(), title : title,
       description : description, complete: false};
 
@@ -116,4 +202,4 @@
   };
 
   if (container) renderState(state);
-})();
+}) ();
